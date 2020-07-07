0ce58cdff0b5068ed5b8322bdc40b6a7982628fc
Jacob Butler <davidson27@gmail.com>
2020-07-07 18:26:39 -0400

    modal window for products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,51 @@
 import React, { Component } from 'react';
 import './CSS/App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Card from './components/productCard';
+import Container from './components/modalContainer/modalContainer'
 import { Grid, Row, Col } from 'react-flexbox-grid';
 
 
-const names = ['James', 'Paul', 'John', 'George', 'Ringo'];
+const apiURL = 'http://localhost:4000/shopping';
 class App extends Component {
+
+  constructor(props)
+  {
+    super(props);
+
+    this.state = {
+      data:[],
+    }
+  }
+
+  async componentDidMount()
+  {
+    const response = await fetch(apiURL)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({data});
+      });
+  }
 
   render() {
     return (
+        <Grid className = "Grid">
+          <Row>
+            <NavBar/>
+          </Row>
 
-      <Grid className = "Grid">
-        <Row>
-          <NavBar/>
-        </Row>
-
-        <Row>
-        {names.map(name => (
-          <Col xs={12} sm={5} lg={3}>
-            <Card/>
-          </Col>
-        ))}
-        </Row>
-      </Grid>
-
-
+          <Row>
+          {this.state.data.map(product => (
+            <Col xs={12} sm={5} lg={3}>
+              <Container triggerText={product.product_name} />
+              <Card product={product}/>
+            </Col>
+          ))}
+          </Row>
+        </Grid>
 
 
-
-
-      // <div className="outerContainer">
-      //   <Row>
-      //     <NavBar/>
-      //   </Row>
-      //
-      //   <div className="productContainer">
-      //     <Row>
-      //       <Col xs />
-      //
-      //       <Col xs />
-      //     </Row>
-      //     <Row>
-      //       <Col xs />
-      //       <Card/><Card/><Card/><Card/>
-      //       <Col xs />
-      //       <Col xs />
-      //     </Row>
-      //   </div>
-      //
-      //
-      //
-      //
-      // </div>
     );
   }
 }
